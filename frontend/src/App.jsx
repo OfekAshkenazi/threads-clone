9cda44f460956be65aa3062b5f2c183baae73949
ofek ashkenazi <ofeka18@gmail.com>
2024-03-31 17:02:49 +0300

    added the basic routes, added photos to public folder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import { Container } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import UserPage from './pages/UserPage'
+import PostPage from './pages/PostPage'
 
 export default function App() {
 
   return (
     <Container maxW={`620px`}>
+      <Routes>
+        <Route path="/:username" element={<UserPage />}/>
+        <Route path="/:username/post/:pid" element={<PostPage />}/>
 
+      </Routes>
     </Container>
   )
 }
