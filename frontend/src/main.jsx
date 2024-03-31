b6b75ca25ea5f528f36aec9b1a7e55d86bd58cd8
ofek ashkenazi <ofeka18@gmail.com>
2024-03-31 17:53:46 +0300

    change the import of the chakra provider for toast will work.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-import { ChakraProvider } from '@chakra-ui/provider';
+import { ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/theme-utils';
 import { ColorModeScript } from "@chakra-ui/color-mode";
 import { mode } from "@chakra-ui/theme-tools";
