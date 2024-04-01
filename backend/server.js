6bdf3ad4ffb556e37b2b941a8e40efa29ce9c994
ofek ashkenazi <ofeka18@gmail.com>
2024-04-01 15:14:09 +0300

    added middleware to parse even nested object.

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(500, () => { console.log(`server running on localhost:${PORT}`) })
