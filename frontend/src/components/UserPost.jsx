b0403c90c261eaad071df3ff3f0899d39e6db463
ofek ashkenazi <ofeka18@gmail.com>
2024-04-01 11:45:59 +0300

    work more on ui of userPost. (its the page of the user with his posts).

--- frontend/src/components/UserPost.jsx
+++ frontend/src/components/UserPost.jsx
@@ -1,25 +1,71 @@
-import { Avatar, Flex, Box } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Avatar, Flex, Box, Text, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { BsThreeDots } from 'react-icons/bs';
 
 
 export default function UserPost() {
     return (
         <Link to={`/ofektheking/post/1`}>
+
             <Flex gap={3} mb={4} py={5} >
+
                 <Flex flexDirection={"column"} alignItems={"center"}>
                     <Avatar size="md" name='ofek ashkenazi' src='/post4.jpg' />
                     <Box w="1px" h={"full"} bg={"gray.light"} my={2}></Box>
-                    <Box position={"relative"} w={"full"}>
-                        <Avatar size={"xs"} name="ofek ashkenazi" src='https://bit.ly/dan-abramov' position={"absolute"} top={"0px"} left={"15px"} padding={"2px"} />
-                        <Avatar size={"xs"} name="ofek ashkenazi" src='https://bit.ly/sage-adebayo' position={"absolute"} bottom={"0px"} right={"-5px"} padding={"2px"} />
-                        <Avatar size={"xs"} name="ofek ashkenazi" src='https://bit.ly/prosper-baba' position={"absolute"} bottom={"0px"} left={"4px"} padding={"2px"} />
+                    <Box position={"relative"} w={"full"} mr={1.5}>
+                        <Avatar
+                            size={"xs"}
+                            name="ofek ashkenazi"
+                            src='https://bit.ly/dan-abramov'
+                            position={"absolute"}
+                            top={"0px"}
+                            left='15px'
+                            padding={"2px"}
+                        />
+                        <Avatar
+                            size={"xs"}
+                            name="ofek ashkenazi"
+                            src='https://bit.ly/sage-adebayo'
+                            position={"absolute"}
+                            bottom={"0px"}
+                            right='-5px'
+                            padding={"2px"}
+                        />
+                        <Avatar
+                            size={"xs"}
+                            name="ofek ashkenazi"
+                            src='https://bit.ly/prosper-baba'
+                            position={"absolute"}
+                            bottom={"0px"}
+                            left='4px'
+                            padding={"2px"}
+                        />
                     </Box>
                 </Flex>
+
                 <Flex flex={1} flexDirection={"column"} gap={2}>
-                    <Flex justifyContent={"space-between"}></Flex>
+                    <Flex justifyContent={"space-between"} w={"full"}>
+                        <Flex w={"full"} alignItems={"center"} >
+                            <Text fontSize={"sm"} fontWeight={"bold"} >ofekashkenazi</Text>
+                            <Image src='/verified.png' w={4} h={4} ml={1} />
+                        </Flex>
+                        <Flex gap={4} alignItems={"center"}>
+                            <Text fontSize={"sm"} color={"gray.light"}>1d</Text>
+                            <BsThreeDots />
+                        </Flex>
+                    </Flex>
+
+                    <Text fontSize={"sm"}>This is a post</Text>
+
+                    <Box borderRadius={6}
+                        overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
+                            <Image src='/post1.webp' w={"full"}/>
+                        </Box>
+
                 </Flex>
 
             </Flex>
+
         </Link>
     )
 }
