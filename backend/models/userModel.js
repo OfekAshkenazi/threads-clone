21cb375d78d66a32891201d5f94bea7e13a604c1
ofek ashkenazi <ofeka18@gmail.com>
2024-04-01 15:48:55 +0300

    added following. to user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,10 @@ const userSchema = mongoose.Schema({
         type: [String],
         default: []
     },
+    following: {
+        type: [String],
+        default: [],
+    },
     bio: {
         type: String,
         default: ""
