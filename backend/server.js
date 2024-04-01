f57ac58d41c53af44e6329c7b0bbd1ac4cb9a8a8
ofek ashkenazi <ofeka18@gmail.com>
2024-04-01 15:13:01 +0300

    added middleware exprees.json for parse json data in the body request.

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,6 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.listen(500,() => {
-    console.log(`server running on localhost:${PORT}`)})
+app.use(express.json())
+
+app.listen(500, () => { console.log(`server running on localhost:${PORT}`) })
