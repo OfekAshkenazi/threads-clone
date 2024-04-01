70c6ac7e99b01c8edf8c52c3b7f2484b2f499c00
ofek ashkenazi <ofeka18@gmail.com>
2024-04-01 15:16:46 +0300

    added middleware cookieparser for add cookie and read it.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/connectDB.js"
+import cookieParser from 'cookie-parser'
+
 dotenv.config()
 
 connectDB()
@@ -9,7 +11,16 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// middlewares
+app.use(express.json()) // parse objects in body req
+app.use(express.urlencoded({ extended: true }))// parse even nested objects in body req
+app.use(cookieParser())
+
+//Routes
+
+// app.use("/api/users", userRoutes)
+
+
+
 
 app.listen(500, () => { console.log(`server running on localhost:${PORT}`) })
