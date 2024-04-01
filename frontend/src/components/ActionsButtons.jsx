1e3e9c6512cfe9018a69a43ee3af08f8f9e719c0
ofek ashkenazi <ofeka18@gmail.com>
2024-04-01 12:51:19 +0300

    work more on the ui of postpage with chakraui.

--- frontend/src/components/ActionsButtons.jsx
+++ frontend/src/components/ActionsButtons.jsx
@@ -5,7 +5,7 @@ import {
 
 export default function ActionsButtons({ liked, setLiked }) {
     return (
-        <Flex gap={3} my={2} onClick={(e) => e.preventDefault()}>
+        <Flex gap={3} my={2} alignItems={"center"} onClick={(e) => e.preventDefault()}>
             <svg
                 aria-label='Like'
                 color={liked ? "rgb(237, 73, 86)" : ""}
@@ -15,6 +15,7 @@ export default function ActionsButtons({ liked, setLiked }) {
                 viewBox='0 0 24 22'
                 width='20'
                 onClick={() => setLiked(!liked)}
+                cursor={"pointer"}
             >
                 <path
                     d='M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z'
@@ -32,6 +33,7 @@ export default function ActionsButtons({ liked, setLiked }) {
                 viewBox='0 0 24 24'
                 width='20'
                 onClick={() => console.log('onopen')}
+                cursor={"pointer"}
             >
                 <title>Comment</title>
                 <path
@@ -62,6 +64,7 @@ const RepostSVG = () => {
             role='img'
             viewBox='0 0 24 24'
             width='20'
+            cursor={"pointer"}
         >
             <title>Repost</title>
             <path
@@ -82,6 +85,7 @@ const ShareSVG = () => {
             role='img'
             viewBox='0 0 24 24'
             width='20'
+            cursor={"pointer"}
         >
             <title>Share</title>
             <line
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,15 +1,63 @@
-import { Avatar, Flex } from "@chakra-ui/react";
+import { Avatar, Flex, Text, Image, Box, Divider, Button } from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
+import ActionsButtons from "../components/ActionsButtons";
+import { useState } from "react";
 
 export default function PostPage() {
+    const [liked, setLiked] = useState(false)
     return (
         <>
-        
-        <Flex>
-            <Flex w={"full"} alignItems={"center"} gap={3}>
-                <Avatar  src="/post4.png" size={"md"} name="ofek ashkenazi"/>
-                <Flex></Flex>
+
+            <Flex>
+
+                <Flex w={"full"} alignItems={"center"} gap={2}>
+                    <Avatar src="/post4.png" size={"md"} name="ofek ashkenazi" />
+
+                    <Flex alignItems={"center"}>
+                        <Text fontSize={"sm"} fontWeight={"bold"}>ofek ashkenazi</Text>
+                        <Image src="/verified.png" w={4} h={4} ml={2} />
+                    </Flex>
+
+                </Flex>
+
+                <Flex gap={4} alignItems={"center"}>
+                    <Text fontSize={"sm"} color={"gray.light"}>1d</Text>
+                    <BsThreeDots />
+                </Flex>
+
             </Flex>
-        </Flex>
+
+            <Text my={3}>Lets talk about one piece</Text>
+
+            <Box borderRadius={6}
+                overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
+                <Image src={"/post1.png"} w={"full"} />
+            </Box>
+
+            <Flex gap={3} my={3}>
+                <ActionsButtons liked={liked} setLiked={setLiked} />
+            </Flex>
+
+            <Flex gap={2} alignItems={"center"}>
+                <Text color={"gray.light"} fontSize={"sm"}>238 replies</Text>
+                <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+                <Text color={"gray.light"} fontSize={"sm"}>
+                    {200 + (liked ? 1 : 0)} likes
+                </Text>
+            </Flex>
+
+            <Divider my={4}/>
+
+            <Flex justifyContent={"space-between"} alignItems={"center"}>
+                <Flex gap={2} alignItems={"center"}>
+                    <Text fontSize={"2xl"}>🤝</Text>
+                    <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+                </Flex>
+                <Button>Get</Button>
+            </Flex>
+
+            <Divider my={4}/>
+
         </>
     )
 }
