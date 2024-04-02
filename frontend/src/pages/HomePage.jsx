498d0faaa8bb65e147deba97c8f598c21ae647ac
ofek ashkenazi <ofeka18@gmail.com>
2024-04-02 23:35:36 +0300

    added show feed of posts to home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,55 @@
 import { Link } from 'react-router-dom';
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, Spinner } from '@chakra-ui/react';
 import { useRecoilValue } from 'recoil';
 import userAtom from './../atoms/user.atom';
+import { useEffect, useState } from 'react';
+import useShowToast from '../hooks/useShowToast';
+import Post from './../components/Post';
 
 export default function HomePage() {
     const user = useRecoilValue(userAtom)
+    const showToast = useShowToast()
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getFeedPosts()
+    }, [])
+
+    async function getFeedPosts() {
+        try {
+            const res = await fetch("/api/posts/feed")
+            const data = await res.json()
+
+            if (data.error) {
+                showToast("Error", data.error, "error")
+            }
+            setPosts(data)
+
+        } catch (error) {
+            showToast("Error", error.message, "error")
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
-        <Link to={`/${user.username}`}>
-            <Flex w={"full"} justifyContent={"center"}>
-                <Button mx={"auto"}>Visit Profile Page</Button>
-            </Flex>
-        </Link>
+        <>
+            {!loading && posts.length === 0 && <h1>Follow some users to see the feed</h1>}
+
+            {loading && (
+                <Flex justifyContent={"center"}>
+                    <Spinner size={"xl"} />
+                </Flex>
+            )}
+
+            {posts.map((post,idx) => {
+                return (
+                    <Post  key={post._id} post={post} postedBy={post.postedBy} />
+                )
+            })}
+
+
+        </>
     )
 }
