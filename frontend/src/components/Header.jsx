b0e17ce26ebe0cb0e437e461d4f7c1cb23d8abc6
ofek ashkenazi <ofeka18@gmail.com>
2024-04-02 15:55:04 +0300

    fix header hight

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     const { colorMode, toggleColorMode } = useColorMode()
 
     return (
-        <Flex justifyContent={"center"} mt={6} mb={12}>
+        <Flex justifyContent={"center"} mt={6} mb={4}>
             <Image cursor={"pointer"} alt="logo" src={colorMode === "dark" ? '/light-logo.svg' : "/dark-logo.svg"} onClick={toggleColorMode} w={6} />
         </Flex>
     )
