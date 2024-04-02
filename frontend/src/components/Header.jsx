c10d95e94e5c1906079a2413453504740cb3a102
ofek ashkenazi <ofeka18@gmail.com>
2024-04-02 20:21:34 +0300

    added when user loggedin btns for going to home page and going to profile page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,29 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Flex, Image, useColorMode, Link } from "@chakra-ui/react";
+import { useRecoilValue } from 'recoil';
+import userAtom from './../atoms/user.atom';
+import { AiFillHome } from 'react-icons/ai'
+import { RxAvatar } from 'react-icons/rx'
+import { Link as RouterLink } from 'react-router-dom'
 
 export default function Header() {
     const { colorMode, toggleColorMode } = useColorMode()
+    const loggedInUser = useRecoilValue(userAtom)
 
     return (
-        <Flex justifyContent={"center"} mt={6} mb={4}>
+        <Flex justifyContent={"space-between"} mt={6} mb={4}>
+
+            {loggedInUser &&
+                <Link as={RouterLink} to="/">
+                    <AiFillHome size={24} />
+                </Link>}
+
             <Image cursor={"pointer"} alt="logo" src={colorMode === "dark" ? '/light-logo.svg' : "/dark-logo.svg"} onClick={toggleColorMode} w={6} />
+
+            {loggedInUser &&
+                <Link as={RouterLink} to={`/${loggedInUser.username}`}>
+                    <RxAvatar size={24} />
+                </Link>}
+
         </Flex>
     )
 }
