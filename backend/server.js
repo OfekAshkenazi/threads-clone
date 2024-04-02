e8b361eed9283001eb05f8dd0835731099c2a778
ofek ashkenazi <ofeka18@gmail.com>
2024-04-02 16:19:33 +0300

    added cloudinary config to my server.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser'
 import userRoutes from './api/user/user.routes.js'
 import postRoutes from './api/post/post.routes.js'
 
+import {v2 as cloudinary} from 'cloudinary'
+
 dotenv.config()
 
 connectDB()
@@ -13,6 +15,13 @@ connectDB()
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:  process.env.CLOUDINARY_API_SECRET 
+})
 
 // middlewares
 app.use(express.json()) // parse objects in body req
