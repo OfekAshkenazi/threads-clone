ed4eb51f7cbed9f3fa5f92179928b61fc6a98a6b
ofek ashkenazi <ofeka18@gmail.com>
2024-04-02 16:56:32 +0300

    fix bug and delete password before send it back to front.

--- backend/api/user/user.controller.js
+++ backend/api/user/user.controller.js
@@ -140,11 +140,13 @@ export async function updateUser(req, res) {
         user.name = name || user.name
         user.email = email || user.email
         user.username = username || user.username
-        user.nprofilePicame = profilePic || user.profilePic
         user.bio = bio || user.bio
+        user.profilePic = profilePic || user.profilePic
 
         user = await user.save()
 
+
+        user.password = null
         res.status(200).json({ message: "Profile updated successfully", user })
 
     } catch (error) {
