09cfcdf5fc6a7cb7bca8c08943da8a4d285a3564
ofek ashkenazi <ofeka18@gmail.com>
2024-04-02 20:07:05 +0300

    change the limit input to server for 50mb

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser'
 import userRoutes from './api/user/user.routes.js'
 import postRoutes from './api/post/post.routes.js'
 
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 
 dotenv.config()
 
@@ -19,12 +19,12 @@ const PORT = process.env.PORT || 5000
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret:  process.env.CLOUDINARY_API_SECRET 
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 // middlewares
-app.use(express.json()) // parse objects in body req
+app.use(express.json({ limit: "50mb" })) // parse objects in body req
 app.use(express.urlencoded({ extended: true }))// parse even nested objects in body req
 app.use(cookieParser())
 
