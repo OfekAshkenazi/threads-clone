da76cdd8778325be6c6cca943480e7ec0b4c4bd0
ofek ashkenazi <ofeka18@gmail.com>
2024-04-03 19:17:10 +0300

    work on the comment cmp. now with real data

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,29 +1,19 @@
 import { Avatar, Divider, Flex, Text } from "@chakra-ui/react"
-import { BsThreeDots } from "react-icons/bs"
-import ActionsButtons from "./ActionsButtons"
 
-export default function Comment({ comment, createdAt, likes, username, userAvatar }) {
+export default function Comment({ reply }) {
 
     return (
         <>
-            <Flex gap={4} py={2} my={2} w={"full"}>
-                <Avatar src={userAvatar} size={"sm"} />
-            </Flex>
-
-            <Flex gap={1} w={"full"} flexDirection={"column"}>
-                <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
-                    <Text fontSize={"sm"} fontWeight={"bold"}>{username}</Text>
-
-                    <Flex gap={2} alignItems={"center"}>
-                        <Text fontSize={"sm"} color={"gray.light"}>{createdAt}</Text>
-                        <BsThreeDots />
+            <Flex gap={4} py={2} my={2} w={"full"} alignItems={"center"}>
+                <Avatar src={reply.userProfilePic} size={"sm"} />
+                <Flex gap={1} w={"full"} flexDirection={"column"}>
+                    <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
+                        <Text fontSize='sm' fontWeight='bold' textDecoration={"underline"}>
+                            {reply.username}
+                        </Text>
                     </Flex>
-
+                    <Text>{reply.text}</Text>
                 </Flex>
-
-                <Text>{comment}</Text>
-                {/* <ActionsButtons  /> */}
-                <Text fontSize={"sm"} color={"gray.light"}> {likes} likes</Text>
             </Flex>
 
             <Divider my={4} />
