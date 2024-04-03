5eddb5cb7fa56078a1719acb882871a1f36f1317
ofek ashkenazi <ofeka18@gmail.com>
2024-04-03 18:26:49 +0300

    added login btn

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,9 @@ import userAtom from './../atoms/user.atom';
 import { AiFillHome } from 'react-icons/ai'
 import { RxAvatar } from 'react-icons/rx'
 import { Link as RouterLink } from 'react-router-dom'
+import { FiLogIn } from "react-icons/fi";
+
+
 
 export default function Header() {
     const { colorMode, toggleColorMode } = useColorMode()
@@ -15,14 +18,23 @@ export default function Header() {
             {loggedInUser &&
                 <Link as={RouterLink} to="/">
                     <AiFillHome size={24} />
-                </Link>}
+                </Link>
+            }
 
             <Image cursor={"pointer"} alt="logo" src={colorMode === "dark" ? '/light-logo.svg' : "/dark-logo.svg"} onClick={toggleColorMode} w={6} />
 
             {loggedInUser &&
                 <Link as={RouterLink} to={`/${loggedInUser.username}`}>
                     <RxAvatar size={24} />
-                </Link>}
+                </Link>
+            }
+
+            {!loggedInUser && (
+                <Link as={RouterLink} to="/auth">
+                    <FiLogIn  size={24} />
+                </Link>
+            )}
+
 
         </Flex>
     )
