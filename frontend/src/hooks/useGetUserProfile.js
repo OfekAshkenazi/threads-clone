55d17d9301250657b7f687a79679f0a79b8d9ee7
ofek ashkenazi <ofeka18@gmail.com>
2024-04-03 19:15:48 +0300

    make an hook that bring me the user of the post. becuz im useing it in 2 places.

---
+++ frontend/src/hooks/useGetUserProfile.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import useShowToast from "./useShowToast"
+import { useParams } from "react-router-dom"
+
+export default function useGetUserProfile() {
+    const [user, setUser] = useState()
+    const [loading, setLoading] = useState(true)
+    const showToast = useShowToast()
+    const { username } = useParams()
+
+    useEffect(() => {
+        getUser()
+
+    }, [username, showToast])
+
+
+    async function getUser() {
+        try {
+            const res = await fetch(`/api/users/profile/${username}`)
+            const data = await res.json()
+
+            if (data.error) {
+                showToast("Error", "Cannot find any user like that", "error")
+                return
+            }
+            setUser(data)
+
+        } catch (error) {
+            showToast("Error", "there was an error while finding this user", "error")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return { loading, user }
+}
