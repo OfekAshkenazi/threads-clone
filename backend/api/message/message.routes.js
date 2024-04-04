8c221a601fa2b1509db98c7f6027cb18312ab90d
ofek ashkenazi <ofeka18@gmail.com>
2024-04-04 20:35:04 +0300

    create first route and logic and put on that route my requireAuth.

--- backend/api/message/message.routes.js
+++ backend/api/message/message.routes.js
@@ -1,6 +1,8 @@
 import express from "express"
+import { requireAuth } from "../../middleware/requireAuth.middleware.js"
+import sendMessage from "./message.controller.js"
 const router = express.Router()
 
-
+router.post("/",requireAuth, sendMessage)
 
 export default router
