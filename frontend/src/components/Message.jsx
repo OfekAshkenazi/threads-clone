53f95f7140517321684082f6cc06a68312091d9e
ofek ashkenazi <ofeka18@gmail.com>
2024-04-04 17:07:51 +0300

    added the msg cmp that will rennder the messages.

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,5 +1,24 @@
-export default function Message() {
+import { Avatar, Flex, Text } from "@chakra-ui/react";
+
+export default function Message({ ownMessage }) {
     return (
-        <section>agaga</section>
+        <>
+            {ownMessage ? (
+                <Flex gap={2} alignSelf={"flex-end"}>
+                    <Text maxW={"350px"} bg={"blue.400"} p={1} borderRadius={"md"}>
+                        loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren
+                    </Text>
+                    <Avatar src="" w="7" h="7" />
+                </Flex>
+            ) : (
+                <Flex gap={2}>
+                    <Avatar src="" w="7" h="7" />
+                    <Text maxW={"350px"} bg={"blue.400"} p={1} borderRadius={"md"}>
+                        loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren loren
+                    </Text>
+                </Flex>
+            )}
+
+        </>
     )
 }
